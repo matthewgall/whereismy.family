375c56fa5f4e435fced169a483fcc19057f19e1b
Matthew Gall <me@matthewgall.com>
2017-12-16 19:59:35 +0000

    And added base scaffolding of the application

---
+++ app.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import os, sys, argparse, logging, json, base64
+import requests, redis
+from bottle import route, request, response, redirect, default_app, view, template, static_file
+
+def enable_cors(fn):
+	def _enable_cors(*args, **kwargs):
+		response.headers['Access-Control-Allow-Origin'] = '*'
+		response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+		response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+		if request.method != 'OPTIONS':
+			return fn(*args, **kwargs)
+	return _enable_cors
+
+@route('/static/<filepath:path>')
+def static(filepath):
+	return static_file(filepath, root='views/static')
+
+
+@route('/', ('GET', 'POST'))
+def index():
+	return "Welcome to whereismy.family"
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+
+	# Server settings
+	parser.add_argument("-i", "--host", default=os.getenv('IP', '127.0.0.1'), help="server ip")
+	parser.add_argument("-p", "--port", default=os.getenv('PORT', 5000), help="server port")
+
+	# Redis settings
+	parser.add_argument("--redis-host", default=os.getenv('REDIS_HOST', 'redis'), help="redis hostname")
+	parser.add_argument("--redis-port", default=os.getenv('REDIS_PORT', 6379), help="redis port")
+	parser.add_argument("--redis-pw", default=os.getenv('REDIS_PW', ''), help="redis password")
+	parser.add_argument("--redis-ttl", default=os.getenv('REDIS_TTL', 60), help="redis time to cache records")
+
+	# Verbose mode
+	parser.add_argument("--verbose", "-v", help="increase output verbosity", action="store_true")
+	args = parser.parse_args()
+
+	if args.verbose:
+		logging.basicConfig(level=logging.DEBUG)
+	else:
+		logging.basicConfig(level=logging.INFO)
+	log = logging.getLogger(__name__)
+
+	try:
+		r = redis.Redis(
+			host=args.redis_host,
+			port=args.redis_port, 
+			password=args.redis_pw,
+		)
+	except:
+		log.error("Unable to connect to redis on {}:{}".format(args.redis_host, args.redis_port))
+
+	try:
+		app = default_app()
+		app.run(host=args.host, port=args.port, server='tornado')
+	except:
+		log.error("Unable to start server on {}:{}".format(args.host, args.port))
